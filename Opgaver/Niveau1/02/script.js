7a9fd30a8da67de6ca5bd9c73a1dd738f3c0b4ee
Loke <U2507983@rts-365.dk>
2025-01-28 11:04:18 +0100

    opgave 2 ekstraopgave løst

--- Opgaver/Niveau1/02/script.js
+++ Opgaver/Niveau1/02/script.js
@@ -7,3 +7,17 @@ function beregnForhold(a, b) {
     console.log(resultat);
 }
 beregnForhold()
+
+function beregnForhold2(a, b) {
+    a = 4;
+    b = 0;
+
+    if (b === 0) {
+        console.log("can't divide by 0");
+        return null
+    } else {
+        let resultat2 = a / b;
+        console.log(resultat2);
+    }
+}
+beregnForhold2()
