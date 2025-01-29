8adea14e51e2ee60eb7aa7c97d4baec4c1106eac
Loke <U2507983@rts-365.dk>
2025-01-29 13:25:58 +0100

    niveau 2 opgave 2 ekstra løst

--- Opgaver/Niveau2/02/script.js
+++ Opgaver/Niveau2/02/script.js
@@ -1,14 +1,25 @@
-const amountButtons = document.querySelectorAll(".product__amount__buttons__container");
+const amountButtons = Array.from(document.querySelectorAll(".product__amount__buttons__container"));
 amountButtons.forEach(element => {
-    element.addEventListener("click", amountPlusMinus);
+	element.addEventListener("click", amountPlusMinus);
 });
 
+const price = Array.from(document.querySelectorAll(".product__price--num"));
+const totalPrice = Array.from(document.querySelectorAll(".product__totalprice--num"));
+
 function amountPlusMinus(e) {
-    if (e.target.classList.contains("product__amount__plus")) {
-        this.previousElementSibling.textContent++;
-    } else if (e.target.classList.contains("product__amount__minus")) {
-        if (this.previousElementSibling.textContent > 0) {
-            this.previousElementSibling.textContent--;
-        }
-    }
+	let productTotal = totalPrice[amountButtons.indexOf(this)]
+	let productCount = this.previousElementSibling
+
+	let productPrice = price[amountButtons.indexOf(this)]
+
+	if (e.target.classList.contains("product__amount__plus")) {
+		productCount.textContent++;
+		productTotal.textContent = productPrice.textContent * productCount.textContent;
+
+	} else if (e.target.classList.contains("product__amount__minus")) {
+		if (productCount.textContent > 0) {
+			productCount.textContent--;
+			productTotal.textContent = productPrice.textContent * productCount.textContent;
+		}
+	}
 }
